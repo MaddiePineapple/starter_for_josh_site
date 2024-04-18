b1d11c7e2a7bce2a5e65e3bbe92e7300b2bcaea2
MaddiePineapple <madisonrosecampbell@icloud.com>
2024-04-18 11:45:54 +0800

    Update footer.php
    made the text editable in worpress

--- footer.php
+++ footer.php
@@ -1,36 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Index" content="Website index page">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet">
-
-
-
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="responsive.css">
-<title>Title</title>
-</head>
-
-<body>
-    <footer class="container-fluid text-center">
+<footer class="container-fluid text-center">
         <div class="container">
             <div class="row">
 
-               <div class="col-md-6">
-                <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+               <div class="col-md-6 widgetleft">
+                    <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a> -->
+                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+                    endif; ?>
                </div> 
 
                <div class="col-md-6">
-                <p>Terms and Conditions</p>
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+                    endif; ?>
+                    <!-- <p>Terms and Conditions</p> -->
                </div> 
 
             </div> <!-- row -->
